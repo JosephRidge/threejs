43ea85fc19f9661d527bd5e1a203320734ae841b
josephridge <josephridge18@gmail.com>
2022-03-04 16:59:44 +0300

    working rotating sphere

--- src/script.js
+++ src/script.js
@@ -43,18 +43,25 @@ scene.add(pointLight)
 
 const pointLight2 = new THREE.PointLight(0xFFFF00, 0.1)
 pointLight2.position.set(1,-1.5,1)
-pointLight2.intensity = 3
-// const pointLightHelper = new THREE.pointLightHelper(pointLight2,1)
+pointLight2.intensity = 5
 scene.add(pointLight2)
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight2,1)
+// scene.add(pointLightHelper2)
 
 gui.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
 gui.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
 gui.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
 
 const pointLight3 = new THREE.PointLight(0x073763, 0.1)
-pointLight3.position.set(5 ,-4.5 ,5 )
-pointLight3.intensity = 3
-// const pointLightHelper = new THREE.pointLightHelper(pointLight2,1)
+pointLight3.position.set(-2.50 ,1.6,-1.39 )
+gui.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
+gui.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
+gui.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
+
+
+pointLight3.intensity = 5
+// const pointLightHelper3 = new THREE.PointLightHelper(pointLight3,1)
+// scene.add(pointLightHelper3)
 scene.add(pointLight3)
 
 /**
@@ -89,6 +96,7 @@ camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 2
 scene.add(camera)
+// scene.background()
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
@@ -101,6 +109,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas, 
     alpha:true
 })
+// renderer.setClearColor(0x073763,0)
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -108,15 +117,34 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+ function onDocumentMouseMove (event)  {
+    mouseX = (event.clientX - windowX)
+    mouseY = (event.clientY - windowY)
+}
+document.addEventListener('mousemove', onDocumentMouseMove)
+
+let mouseX = 0 
+let mouseY = 0
+let targetX = 0
+let targetY = 0
+
+const windowX = window.innerHeight /2;
+const windowY = window.innerWidth /2;
+
+
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
 
+targetX = mouseX * .001 ; 
+targetY = mouseY *.001 ; 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.y += .5 * (targetX -sphere.rotation.y) 
+    sphere.rotation.x += .5 * (targetX -sphere.rotation.x) 
 
     // Update Orbital Controls
     // controls.update()
