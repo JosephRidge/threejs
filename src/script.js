872e083044feb762f6a4d76e3067a5101c7b805b
josephridge <josephridge18@gmail.com>
2022-03-04 23:49:40 +0300

    Flow working well

--- src/script.js
+++ src/script.js
@@ -3,6 +3,185 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+
+//Textureloader 
+const textureLoader = new THREE.TextureLoader()
+const cross = textureLoader.load('/textures/normalMap.png')
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+
+const particlesGeometry = new THREE.BufferGeometry;
+const paritclesCnt = 5000
+
+const posArray = new Float32Array(paritclesCnt * 3)
+
+for(let i = 0 ; i<paritclesCnt*3 ; i++){
+    posArray[i] = (Math.random()-0.5)* 5 
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3))
+
+// Materials
+
+
+const material = new THREE.PointsMaterial({
+    
+    size:0.005,
+     map:cross,
+    transparent:true,
+    color:'aqua',
+    blending:THREE.AdditiveBlending
+})
+
+// material.color = new THREE.Color(0xff0000)
+
+// Mesh
+const sphere = new THREE.Points(geometry,material)
+const particlesMesh = new THREE.Points(particlesGeometry, material)
+scene.add(sphere, particlesMesh)
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setClearColor(new THREE.Color("#21282a"),1)
+
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+let mouseX = 0 
+let mouseY = 0
+let targetX = 0
+let targetY = 0
+const windowX = window.innerHeight /2;
+const windowY = window.innerWidth /2;
+document.addEventListener('mousemove', animateParticles)
+function animateParticles (event)  {
+    mouseX = (event.clientX )
+    mouseY = (event.clientY )
+}
+
+
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+    //particlesMesh.rotation.x = mouseX  *(elapsedTime*0.0008)
+    particlesMesh.rotation.y = -.1 *(elapsedTime*0.0008)
+
+    // Update objects
+     sphere.rotation.y = .5 * elapsedTime
+     if(mouseX>0){
+    particlesMesh.rotation.x = mouseX  *(elapsedTime*0.00008)
+    particlesMesh.rotation.y = mouseY  *(elapsedTime*0.00008)
+  
+     }
+    // let position += y ;
+    //  camera.position.y = position
+    
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
 // Debug
 const gui = new dat.GUI()
 
@@ -48,15 +227,15 @@ scene.add(pointLight2)
 // const pointLightHelper2 = new THREE.PointLightHelper(pointLight2,1)
 // scene.add(pointLightHelper2)
 
-gui.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
-gui.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
-gui.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
+// gui.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
+// gui.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
+// gui.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
 
 const pointLight3 = new THREE.PointLight(0x073763, 0.1)
 pointLight3.position.set(-2.50 ,1.6,-1.39 )
-gui.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
-gui.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
-gui.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
+// gui.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
+// gui.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
+// gui.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
 
 
 pointLight3.intensity = 5
@@ -66,7 +245,7 @@ scene.add(pointLight3)
 
 /**
  * Sizes
- */
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -89,7 +268,7 @@ window.addEventListener('resize', () =>
 
 /**
  * Camera
- */
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
@@ -104,7 +283,7 @@ scene.add(camera)
 
 /**
  * Renderer
- */
+
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas, 
     alpha:true
@@ -115,7 +294,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
- */
+
 
  function onDocumentMouseMove (event)  {
     mouseX = (event.clientX - windowX)
@@ -157,3 +336,4 @@ targetY = mouseY *.001 ;
 }
 
 tick()
+*/
