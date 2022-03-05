121dc1d7ee7335f105aaaac7b2b8681e4035f242
josephridge <josephridge18@gmail.com>
2022-03-05 11:25:46 +0300

    Updates

---
+++ src/secrets.js
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics"; 
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDCHFx4vmcS8ap50304OLO_gtN2F4ig49E",
+//   authDomain: "iotlab2021dev1.firebaseapp.com",
+//   databaseURL: "https://iotlab2021dev1-default-rtdb.firebaseio.com",
+//   projectId: "iotlab2021dev1",
+//   storageBucket: "iotlab2021dev1.appspot.com",
+//   messagingSenderId: "815978701320",
+//   appId: "1:815978701320:web:8fea9c91d06ad0b93867bd",
+//   measurementId: "G-N52RW1WDHJ"
+// };
+
+// // Initialize Firebase
+// // const analytics = getAnalytics(app);
